469ecf9ef746a6dcce21a3c3eb2f80c3019daab3
Amighi Thomas <thomas.amighi@hotmail.com>
2025-11-12 16:08:37 +0100

    refactor: simplify useControls

--- src/hooks/useControls/useControls.tsx
+++ src/hooks/useControls/useControls.tsx
@@ -1,28 +1,19 @@
 import type { ControlLeaf } from "@/types/leaf";
 import type { AppControlPath } from "@/types/path";
 import React from "react";
-import { useControlsContext } from "./useControlsContext";
 import { useControlsSubscription } from "./useControlsSubscription";
-import { getControlsByPath } from "./utils";
 
 export const useControls = <const T extends AppControlPath = "">(
     path: T = "" as T,
 ): ControlLeaf<T> => {
-    const { get } = useControlsContext();
-
-    const initialControls = React.useMemo(
-        () => getControlsByPath(get(), path),
-        [],
-    );
-
-    const [controls, setControls] =
-        React.useState<ControlLeaf<T>>(initialControls);
-
     const callback = React.useCallback((newControls: ControlLeaf<T>) => {
         setControls(newControls);
     }, []);
 
-    useControlsSubscription(path, callback);
+    const initialControls = useControlsSubscription(path, callback);
+
+    const [controls, setControls] =
+        React.useState<ControlLeaf<T>>(initialControls);
 
     return controls;
 };
