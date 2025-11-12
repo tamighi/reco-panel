c4627cf9268520781c6bf3ffa9aca6f0aa6472c7
Amighi Thomas <thomas.amighi@hotmail.com>
2025-11-12 15:36:52 +0100

    fix: remove test tree

--- src/types/register.ts
+++ src/types/register.ts
@@ -1,29 +1,7 @@
 export interface Register {}
 
-const testControlTree = {
-    fold1: {
-        x: {
-            value: 5,
-        },
-        y: 2,
-        foldRec: {
-            z: "",
-        },
-    },
-    fold3: {
-        w: {
-            value: 17,
-            min: 2,
-            step: 1,
-            max: 3,
-        },
-    },
-};
-
-export type RegisteredControlTree = typeof testControlTree;
-
-// export type RegisteredControlTree<TRegister = Register> = TRegister extends {
-//     controlTree: infer Tree;
-// }
-//     ? Tree
-//     : any;
+export type RegisteredControlTree<TRegister = Register> = TRegister extends {
+    controlTree: infer Tree;
+}
+    ? Tree
+    : any;
